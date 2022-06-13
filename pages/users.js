a44954dea6354a71eb59e5d062478d8858bbdb1f
Nhật Vũ <nhatvu148@gmail.com>
2022-06-13 11:43:47 +0900

    static gen with getStaticProps

---
+++ pages/users.js
@@ -0,0 +1,29 @@
+const UserList = ({ users }) => {
+  return (
+    <>
+      <h1>List of users</h1>
+      {users.map((user) => {
+        return (
+          <div key={user.id}>
+            <p>{user.name}</p>
+            <p>{user.email}</p>
+          </div>
+        );
+      })}
+    </>
+  );
+};
+
+export default UserList;
+
+export const getStaticProps = async () => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await response.json();
+  console.log(data);
+
+  return {
+    props: {
+      users: data,
+    },
+  };
+};

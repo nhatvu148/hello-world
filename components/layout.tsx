4fb6fe222ecf39ae61922411a732653c77ef0ff1
Nhật Vũ <nhatvu148@gmail.com>
2022-06-16 18:16:05 +0900

    custom layout

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,8 +1,14 @@
+import Head from "next/head";
 import SectionNavbars from "pages-sections/components/SectionNavbars.js";
 
 export default function Layout({ children }) {
   return (
     <>
+      <Head>
+        <title>Next Movies</title>
+        <meta name="description" content="Next Movies" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <SectionNavbars />
       {children}
     </>
--- pages/about.tsx
+++ pages/about.tsx
@@ -1,5 +1,15 @@
+import Head from "next/head";
+
 const About = () => {
-  return <h1>About page</h1>;
+  return (
+    <>
+      <Head>
+        <title>About</title>
+        <meta name="description" content="About"></meta>
+      </Head>
+      <h1>About page</h1>
+    </>
+  );
 };
 
 export default About;
